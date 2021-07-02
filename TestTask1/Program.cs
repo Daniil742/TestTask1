8dcb1305f5aa4564d93407207240f351354cc4f0
Timofeev Daniil <timofeev742@gmail.com>
2021-07-02 20:51:11 +0400

    Third

--- TestTask1/Program.cs
+++ TestTask1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TestTask1
@@ -10,12 +10,43 @@ namespace TestTask1
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your password: ");
-            string password = Console.ReadLine();
+            PasswordValidation();
+            Console.ReadKey();
         }
 
-        private void PasswordValidation(string pass)
+        private static void PasswordValidation()
         {
+            string valid = "abcdefghijklmnopqrstuvwxyz0123456789.-_";
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            char[] validArray = valid.ToCharArray();
+
+            while (true)
+            {
+                Console.Write("Enter your password: ");
+                string password = Console.ReadLine();
+                //bool tmp = false;
+
+                if (password.Length >= 8 && password.Length <= 25 && alphabet.Contains(password.ToLower().First()) && alphabet.Contains(password.ToLower().Last()))
+                {
+                    char[] passArray = password.ToLower().ToCharArray();
+                    for (int i = 0; i < passArray.Length; i++)
+                    {
+                        if (!valid.Contains(passArray[i].ToString()))
+                        {
+                            //tmp = true;
+                            Console.WriteLine("Invalid password. Try again.");
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Password confirmed.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password. Try again.");
+                }
+            }
         }
+
     }
 }
